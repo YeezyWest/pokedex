85fc21e4dca14e77b582aa706f232ff7c71092e5
Yusuf olatunji adeshina <yadeshina57@gmail.com>
2026-02-19 14:58:54 +0100

    feat: Implement fetching and displaying a list of Pokémon from PokeAPI.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function Index() {
+const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+useEffect(() => {
+  fetchPokemon();
+}, []);
+
+const fetchPokemon = async () => {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data = await response.json();
+
+//fetch detailed info for each pokemon in parallel
+  const detailedPokemonData = await Promise.all(
+    data.results.map(async (pokemon: any) => {
+      const res = await fetch(pokemon.url);
+      const details = await res.json();
+      return {
+        name: pokemon.name,
+        url: pokemon.url,
+        image: details.sprites.front_default,
+      };
+    })
+  );
+
+    console.log(detailedPokemonData);
+    setPokemon(data.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
-    </View>
+    <ScrollView>
+     {pokemon.map((pokemon) => (
+      <View key={pokemon.name}>
+        <Text>{pokemon.name}</Text>
+      </View>
+     ))}
+    </ScrollView>
   );
 }
